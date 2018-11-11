06346fcb43217c93e08cc960f46bc8eac9206339
Samantha F McCabe <fiona.morrigan@gmail.com>
2018-11-11 18:10:53 -0500

    [didelphis-sca-gui-17] Switch to new architecture

---
+++ src/main/java/org/didelphis/soundchange/ui/controller/ViewController.java
@@ -0,0 +1,12 @@
+package org.didelphis.soundchange.ui.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ViewController {
+
+//	@RequestMapping ("/")
+//	String home() {
+//		return "Hello World!";
+//	}
+}
