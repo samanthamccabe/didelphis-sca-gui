878d74bed7037183ef0fa464967e69f9fe74bf3d
Samantha F Mccabe <samantha.mccabe@didelphis.org>
2018-12-23 13:42:11 -0500

    [didelphis-sca-gui-20] Open and Close Projects

--- sca-ui-resources/src/main/resources/view.js
+++ sca-ui-resources/src/main/resources/view.js
@@ -1,3 +1,23 @@
+/******************************************************************************
+ * Didelphis Grammatekton - an graphical development environment for          *
+ * constructed language development and sound-change rule application         *
+ *                                                                            *
+ * Copyright (C) 2016 Samantha F McCabe                                       *
+ *                                                                            *
+ * This program is free software: you can redistribute it and/or modify       *
+ * it under the terms of the GNU General Public License as published by       *
+ * the Free Software Foundation, either version 3 of the License, or          *
+ * (at your option) any later version.                                        *
+ *                                                                            *
+ * This program is distributed in the hope that it will be useful,            *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+ * GNU General Public License for more details.                               *
+ *                                                                            *
+ * You should have received a copy of the GNU General Public License          *
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.     *
+ ******************************************************************************/
+
 let logView  = null;
 let fileTree = null;
 
